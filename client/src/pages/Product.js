bcdd17f5883caed840925a75d63a36084f120309
Saksham Gupta <misakshamgupta@gmail.com>
2025-02-20 00:33:21 +0530

    Update Product.js

---
+++ client/src/pages/Product.js
@@ -0,0 +1,9 @@
+// src/pages/product/[id].js
+import React from 'react';
+import ProductDetail from '../../components/ProductDetail';
+
+const Product = () => {
+  return <ProductDetail />;
+};
+
+export default Product;
