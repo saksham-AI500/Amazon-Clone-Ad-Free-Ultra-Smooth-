05c829e2b60bede3e00d27b0b84c5bc4e3690f09
Saksham Gupta <misakshamgupta@gmail.com>
2025-02-20 00:36:30 +0530

    Update auth.js

---
+++ server/routes/auth.js
@@ -0,0 +1,28 @@
+// server/routes/auth.js
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+  res.send('User registered');
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.send({ token });
+  } else {
+    res.status(401).send('Invalid credentials');
+  }
+});
+
+module.exports = router;
