f56b4389897546987ff9a1b59d4106f9429df51d
Saksham Gupta <misakshamgupta@gmail.com>
2025-02-20 00:34:31 +0530

    Update User.js

---
+++ server/models/User.js
@@ -0,0 +1,11 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
